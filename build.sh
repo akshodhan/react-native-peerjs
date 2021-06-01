5db3f91d773c0aa5cb0c6e54707b08b2a22e9dc7
Abhishek Shodhan <abhishek.shodhan@discus.solutions>
2021-06-01 18:12:47 +0530

    Fixed cat command & dist directory locations.

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 exec 2>&1
 
 rm -rf dist/ peerjs/
+mkdir dist
 git clone https://github.com/peers/peerjs peerjs
 cd peerjs
 git apply ../decoupling.diff
 ../node_modules/.bin/parcel build --no-source-maps lib/exports.ts -d ../dist --out-file peerjs.min.js
 cd ../
-cat imports.js dist/peerjs.min.js > dist/react-native-peerjs.js
-rm dist/peerjs.min.js
+cat imports.js peerjs/dist/peerjs.min.js > dist/react-native-peerjs.js
+rm peerjs/dist/peerjs.min.js
 
 echo "Done. dist/react-native-peerjs.js"
-
